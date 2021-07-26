466c8005b408019b50499397192f322d254df920
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-07-26 00:16:15 -0300

    combining reducer favoritesReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
+import favoritesReducer from "./favorites.reducer";
 import pokemonReducer from "./pokemon.reducer";
 
 const rootReducer = combineReducers({
   pokemon: pokemonReducer,
+  favorites: favoritesReducer,
 });
 
 export default rootReducer;
