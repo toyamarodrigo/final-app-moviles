a79e8659434985c6bea67a6c77d4c7cb8a679c4c
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-08-02 01:00:50 -0300

    styles: add PokeCard and favorite implementantion on Details

--- src/components/PokeCard/PokeCard.jsx
+++ src/components/PokeCard/PokeCard.jsx
@@ -1,178 +1,183 @@
+import React, { useState, useEffect } from "react";
+import { Image, Text, Box, VStack, HStack, Stack, Button, Icon } from "native-base";
 import { StyleSheet } from "react-native";
-import { Image, Text, View } from "native-base";
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FontAwesome } from "@expo/vector-icons";
 
-export const PokeCard = () => {
+import { addFavorite, removeFavorite } from "../../actions/favorites.actions";
+
+export const PokeCard = ({ currentPokemon, favoritePokemons }) => {
+  const { favorites } = useSelector((state) => state);
+  const [favoriteButton, setFavoriteButton] = useState(null);
+  const dispatch = useDispatch();
+
+  const handleFavorites = (pokemonId) => {
+    setFavoriteButton(!favoriteButton);
+    if (favoriteButton) {
+      dispatch(removeFavorite(pokemonId));
+    } else {
+      dispatch(addFavorite(pokemonId));
+    }
+  };
+
+  useEffect(() => {
+    const result = favorites.favoritePokemons?.some(
+      (favPokemon) => favPokemon.fav_pokemon.id === currentPokemon.id,
+    );
+
+    setFavoriteButton(result);
+  }, []);
+
   return (
     <>
-      <View style={styles.pokemonBodyContainer}>
-        {/* Pokemon Image */}
-        <Image alt="pokemon image" src="#" style={styles.pokemonImage} />
-
-        {/* Pokemon Order & Name */}
-        <View style={styles.pokemonNameContainer}>
-          <Text>#</Text>
-          <Text>#</Text>
-        </View>
-
-        {/* Pokemon HP, ATK, DF, SP & Type */}
-        <View style={styles.pokemonInfo}>
-          <View style={styles.pokemonStatsContainer}>
-            <View
-              style={{
-                display: "flex",
-                gap: "10px",
-                flexDirection: "column",
-                textAlign: "justify",
-              }}
-            >
-              <Text>{`HP: 100`}</Text>
-              <Text>{`ATK: 100`}</Text>
-              <Text>{`DF: 100`}</Text>
-              <Text>{`SP: 100`}</Text>
-            </View>
-          </View>
-          <View style={styles.pokemonTypesContainer}>
-            <Text>{`Type:`}</Text>
-          </View>
-        </View>
-
-        {/* Pokemon Base EXP, Height & Weight */}
-        <View style={styles.pokemonStatsContainer}>
-          <View>
-            <View style={styles.pokemonSquareStat}>
-              <Text>BASE EXP</Text>
-            </View>
-
-            <View style={styles.pokemonSquareTitle}>
-              <Text>Base Exp.</Text>
-            </View>
-          </View>
-
-          <View>
-            <View style={styles.pokemonSquareStat}>
-              <Text>HEIGHT mt</Text>
-            </View>
-
-            <View style={styles.pokemonSquareTitle}>
-              <Text>Height</Text>
-            </View>
-          </View>
-
-          <View>
-            <View style={styles.pokemonSquareStat}>
-              <Text>WEIGHT kg</Text>
-            </View>
-
-            <View style={styles.pokemonSquareTitle}>
-              <Text>Weight</Text>
-            </View>
-          </View>
-        </View>
-      </View>
+      <Box
+        alignItems="center"
+        backgroundColor="#fefefe"
+        borderRadius={10}
+        h={"auto"}
+        justifyContent="center"
+        position="relative"
+        w={"90%"}
+      >
+        <Box position="absolute" right={5} top={5} zIndex={2}>
+          <Button
+            backgroundColor="white"
+            borderRadius="full"
+            size={10}
+            onPress={() => handleFavorites(currentPokemon.id)}
+          >
+            <Icon
+              as={FontAwesome}
+              color={favoriteButton ? "red" : "black"}
+              name={favoriteButton ? "heart" : "heart-o"}
+              size="sm"
+            />
+          </Button>
+        </Box>
+        <VStack
+          borderTopRadius={10}
+          pb={4}
+          style={styles(currentPokemon.types[0].type.name).pokemonColorType}
+          w={"100%"}
+        >
+          <Stack alignItems="center" justifyContent="center" py={4} space={4} w={"100%"}>
+            <HStack fontSize={20} space={4}>
+              <Text fontWeight="bold" style={styles(currentPokemon.types[0].type.name).pokemonName}>
+                # {currentPokemon.order}
+              </Text>
+              <Text
+                fontWeight="bold"
+                style={styles(currentPokemon.types[0].type.name).pokemonName}
+                textTransform="capitalize"
+              >
+                {currentPokemon.name}
+              </Text>
+            </HStack>
+            <Image
+              alt="pokemon image"
+              backgroundColor="white"
+              borderRadius="full"
+              borderWidth={2}
+              h={120}
+              source={{ uri: currentPokemon.sprites.front_default }}
+              w={120}
+            />
+          </Stack>
+          <Stack direction="column" space={4}>
+            <Stack direction="row" justifyContent="center" space={2}>
+              {currentPokemon.types.map((pokemon, index) => (
+                <Box key={index}>
+                  <Image
+                    alt="Pokemon badge type"
+                    height={4}
+                    source={{
+                      uri: `https://veekun.com/dex/media/types/en/${pokemon.type.name}.png`,
+                    }}
+                    width={50}
+                  />
+                </Box>
+              ))}
+            </Stack>
+          </Stack>
+        </VStack>
+        <VStack backgroundColor="orange" borderBottomRadius={10} pb={6} space={2} w={"100%"}>
+          <HStack alignItems="center" my={4}>
+            <HStack alignItems="center" justifyContent="center" space={8} w={"100%"}>
+              <VStack space={2} w={"25%"}>
+                <Text>
+                  <Text fontWeight="bold">HP: </Text>
+                  {currentPokemon.stats[0].base_stat}
+                </Text>
+                <Text>
+                  <Text fontWeight="bold">ATK: </Text>
+                  {currentPokemon.stats[1].base_stat}
+                </Text>
+                <Text>
+                  <Text fontWeight="bold">DF: </Text>
+                  <Text>{currentPokemon.stats[2].base_stat}</Text>
+                </Text>
+                <Text>
+                  <Text fontWeight="bold">SP: </Text>
+                  {currentPokemon.stats[3].base_stat}
+                </Text>
+              </VStack>
+              <VStack
+                alignItems="center"
+                justifyContent="center"
+                space={4}
+                textAlign="center"
+                w={"25%"}
+              >
+                <Text>
+                  <Text fontWeight="bold">Height: </Text>
+                  {currentPokemon.height / 10}mt
+                </Text>
+                <Text>
+                  <Text fontWeight="bold">Weight: </Text>
+                  {currentPokemon.weight / 10}kg
+                </Text>
+              </VStack>
+            </HStack>
+          </HStack>
+          <VStack alignItems="center">
+            <Text>
+              <Text fontWeight="bold">BASE EXP: </Text>
+              {currentPokemon.base_experience}
+            </Text>
+          </VStack>
+        </VStack>
+      </Box>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  pokemonImage: {
-    position: "absolute",
-    width: 100,
-    height: "auto",
-    backgroundColor: "white",
-    borderRadius: 50,
-    top: "-15%",
-  },
-  pokemonBodyContainer: {
-    position: "relative",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fefefe",
-    borderRadius: 20,
-    width: "100%",
-  },
-  pokemonNameContainer: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    fontSize: 18,
-    textTransform: "capitalize",
-  },
-  pokemonInfo: {
-    display: "flex",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    width: "100%",
-  },
-  pokemonTypesContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 15,
-  },
-  pokemonStatsContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 24,
-  },
-  pokemonSquareStat: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-    borderRadius: 5,
-    width: 100,
-  },
-  pokemonSquareTitle: {
-    marginTop: 10,
-  },
-});
-
-// const PokemonTypesContainer = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   gap: 20px;
-//   margin-top: 15px;
-// > span {
-//   img {
-//     height: 20px;
-//     width: auto;
-//   }
-// }
-// `;
-
-// const PokemonStatsContainer = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   margin-top: 24px;
-//   > div {
-//     text-align: center;
-//     > h3 {
-//       font-size: 10px;
-//     }
-//   }
-// `;
-
-// const PokemonSquareStat = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   padding: 20px;
-//   border-radius: 5px;
-//   width: 100px;
-//   h3 {
-//     font-size: 14px;
-//   }
-// `;
-
-// const PokemonSquareTitle = styled.div`
-//   margin-top: 10px;
-//   h3 {
-//     font-size: 10px;
-//   }
-// `;
+const styles = (colorType) =>
+  StyleSheet.create({
+    pokemonColorType: {
+      backgroundColor:
+        (colorType === "bug" && "#A8B820") ||
+        (colorType === "dark" && "#7C4A16") ||
+        (colorType === "dragon" && "#603986") ||
+        (colorType === "electric" && "#FFD700") ||
+        (colorType === "fairy" && "#F8B2E7") ||
+        (colorType === "fighting" && "#8E3931") ||
+        (colorType === "fire" && "#F05030") ||
+        (colorType === "flying" && "#B280D2") ||
+        (colorType === "ghost" && "#43275D") ||
+        (colorType === "grass" && "#137C15") ||
+        (colorType === "ground" && "#E0C068") ||
+        (colorType === "ice" && "#728EF0") ||
+        (colorType === "normal" && "#A8A878") ||
+        (colorType === "poison" && "#A040A0") ||
+        (colorType === "psychic" && "#D05D7A") ||
+        (colorType === "rock" && "#745600") ||
+        (colorType === "shadow" && "#F45987") ||
+        (colorType === "steel" && "#B8B8D0") ||
+        (colorType === "water" && "#A9DEf2") ||
+        "#9BE5DC",
+    },
+    pokemonName: {
+      color:
+        colorType === "dark" || colorType === "dragon" || colorType === "ghost" ? "white" : "black",
+    },
+  });
--- src/screens/Details.jsx
+++ src/screens/Details.jsx
@@ -1,65 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { Box, Center, Stack, Text, Image, Button, Icon } from "native-base";
-import { useDispatch, useSelector } from "react-redux";
-import { FontAwesome } from "@expo/vector-icons";
+import React from "react";
+import { Stack } from "native-base";
+import { useSelector } from "react-redux";
 
-import { addFavorite, removeFavorite } from "../actions/favorites.actions";
 import { PokeCard } from "../components";
 import { BasicLayout } from "../layout";
 
 export const Details = () => {
-  const [favoriteButton, setFavoriteButton] = useState(null);
-  const { pokemon, favorites } = useSelector((state) => state);
-
-  const dispatch = useDispatch();
-
-  const handleFavorites = (pokemonId) => {
-    setFavoriteButton(!favoriteButton);
-    if (favoriteButton) {
-      dispatch(removeFavorite(pokemonId));
-    } else {
-      dispatch(addFavorite(pokemonId));
-    }
-  };
-
-  useEffect(() => {
-    const result = favorites.favoritePokemons.some(
-      (favPokemon) => favPokemon.fav_pokemon.id === pokemon.currentPokemon.id,
-    );
-
-    setFavoriteButton(result);
-  }, []);
-
-  // TODO: useEffect to get Favorites from localStorage and display data
+  const { pokemon } = useSelector((state) => state);
 
   return (
     <BasicLayout>
       <Stack safeArea alignItems="center" justifyContent="center" space={4} w={"100%"}>
-        <Center>POKEMON DETAIL</Center>
-        {/* <Box
-          alignItems="center"
-          backgroundColor="#fefefe"
-          borderRadius={20}
-          direction="column"
-          display="flex"
-          justifyContent="center"
-          position="relative"
-          width="90%"
-        >
-          <Image
-            alt="pokemon image"
-            size={"lg"}
-            source={{ uri: pokemon.currentPokemon.sprites.front_default }}
-          />
-          <Text textAlign="center">{pokemon.currentPokemon.name}</Text>
-
-          <Icon
-            as={FontAwesome}
-            name={favoriteButton ? "heart" : "heart-o"}
-            onPress={() => handleFavorites(pokemon.currentPokemon.id)}
-          />
-        </Box> */}
-        <PokeCard />
+        <PokeCard currentPokemon={pokemon.currentPokemon} />
       </Stack>
     </BasicLayout>
   );
